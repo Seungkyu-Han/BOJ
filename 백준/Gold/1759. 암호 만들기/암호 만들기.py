71f5e27fd54e70ed41f871ff14992d2f9b247b62
Seungkyu-Han <98071131+Seungkyu-Han@users.noreply.github.com>
2024-01-15 18:24:43 +0900

    [Gold V] Title: 암호 만들기, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,25 +1,25 @@
 import sys
 
 L, C = map(int, sys.stdin.readline().split())
-words = sorted(list(sys.stdin.readline().split()))
 
-vowels = ['a', 'e', 'i', 'o', 'u']
+candidate = sorted(list(map(str, sys.stdin.readline().split())))
 
-
-def backtracking(cur_words, cur_cnt, cur_index):
-    if cur_cnt == L:
-        v_count, c_count = 0, 0
-        for i in cur_words:
-            if i in vowels:
-                v_count += 1
-            else:
-                c_count += 1
-        if v_count > 0 and c_count > 1:
-            print(''.join(cur_words))
-        return
-
-    for i in range(cur_index, C):
-        backtracking(cur_words + [words[i]], cur_cnt + 1, i + 1)
+important = {'a', 'e', 'i', 'o', 'u'}
 
 
-backtracking([], 0, 0)
+def make_key(cur_candidate, index):
+
+    if index == C:
+        value = len(set(cur_candidate).intersection(important))
+        if len(cur_candidate) == L and value >= 1 and (L - value) >= 2:
+            for i in cur_candidate:
+                print(i, end='')
+            print()
+            return
+
+    else:
+        make_key(cur_candidate + [candidate[index]], index + 1)
+        make_key(cur_candidate, index + 1)
+
+
+make_key([], 0)
