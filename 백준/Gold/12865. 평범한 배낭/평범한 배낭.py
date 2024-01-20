39e59ce38bc688a5310e159e76b7f0aec616cf72
Seungkyu-Han <98071131+Seungkyu-Han@users.noreply.github.com>
2024-01-20 10:18:35 +0900

    [Gold V] Title: 평범한 배낭, Time: 3684 ms, Memory: 34972 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,21 +1,19 @@
 import sys
 
 N, K = map(int, sys.stdin.readline().split())
-dp = [[0] * (K + 1) for i in range(N + 1)]
-thing = [[0, 0]]
+
+candidate_weight = []
+candidate_value = []
 
 for i in range(N):
-    thing.append(list(map(int, sys.stdin.readline().split())))
+    W, V = map(int, sys.stdin.readline().split())
+    candidate_weight.append(W)
+    candidate_value.append(V)
 
-for i in range(1, N + 1):
-    w = thing[i][0]
-    v = thing[i][1]
-    for t in range(1, K + 1):
-        if t < w:
-            dp[i][t] = max(dp[i-1][t], dp[i][t-1])
-        else:
-            dp[i][t] = max(dp[i-1][t], dp[i][t-1],dp[i-1][t-w] + v)
+result = [0 for i in range(K + 1)]
 
+for i in range(N):
+    for t in range(K, candidate_weight[i] - 1, -1):
+        result[t] = max(result[t], result[t-candidate_weight[i]] + candidate_value[i])
 
-
-print(dp[-1][-1])
+print(max(result))
