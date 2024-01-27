96f9592ac6c57e665c7e66514db8a8573c875a91
Seungkyu-Han <98071131+Seungkyu-Han@users.noreply.github.com>
2024-01-27 16:58:54 +0900

    [Gold V] Title: 전깃줄, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.py
+++ 백준/Gold/2565. 전깃줄/전깃줄.py
@@ -1,32 +1,16 @@
 import sys
 
-cnt = int(sys.stdin.readline())
+n = int(sys.stdin.readline().strip())
 
-line = [[0, 0]] * cnt
+line = sorted([list(map(int, sys.stdin.readline().split())) for _ in range(n)])
 
-for i in range(cnt):
-    line[i] = list(map(int, sys.stdin.readline().split()))
+dp = [0 for i in range(n)]
 
-line.sort()
+for i in range(n):
+    count = 1
+    for t in range(i):
+        if line[i][1] > line[t][1]:
+            count = max(count, dp[t] + 1)
+    dp[i] = count
 
-result = []
-
-for i in range(cnt):
-    tmp_max = 1
-    tmp_result = [[1, [i]]]
-    for tmp_cnt, tmp_list in result:
-        flag = True
-        for t in tmp_list:
-            if (line[i][0] - line[t][0]) * (line[i][1] - line[t][1]) <= 0:
-                flag = False
-                break
-        if flag:
-            if tmp_max == tmp_cnt + 1:
-                tmp_result.append([tmp_cnt + 1, tmp_list + [i]])
-            elif tmp_max < tmp_cnt + 1:
-                tmp_max = tmp_cnt + 1
-                tmp_result = [[tmp_cnt + 1, tmp_list + [i]]]
-    result.extend(tmp_result)
-
-result.sort()
-print(cnt - result[-1][0])
+print(n - max(dp))
