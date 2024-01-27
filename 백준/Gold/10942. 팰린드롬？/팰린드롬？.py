7d84e89ca50af6038cec7bcfa8be373a88493b58
Seungkyu-Han <98071131+Seungkyu-Han@users.noreply.github.com>
2024-01-27 22:21:35 +0900

    [Gold IV] Title: 팰린드롬?, Time: 792 ms, Memory: 282436 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
@@ -1,47 +1,32 @@
 import sys
 
+sys.setrecursionlimit(100000)
+
 N = int(sys.stdin.readline())
 
-nums = list(map(int, sys.stdin.readline().split()))
+palindrome = list(map(int, sys.stdin.readline().split()))
 
-result = [[-1] * N for i in range(N)]
+dp = [[0 for i in range(N)] for t in range(N)]
+
+for i in range(N):
+    dp[i][i] = 2
 
 
-def judge(start, end):
-    if result[start][end] != -1:
-        return result[start][end]
-
-    if start == end:
-        if nums[start] == nums[end]:
-            result[start][end] = 1
-            return 1
-        else:
-            result[start][end] = 0
-            return 0
-
-    if end - start == 1:
-        if nums[start] == nums[end]:
-            result[start][end] = 1
-            return 1
-        else:
-            result[start][end] = 0
-            return 0
-
-    if nums[start] == nums[end]:
-        result[start][end] = judge(start + 1, end - 1)
-        return result[start][end]
+def palindrome_dp(start, end):
+    if dp[start][end] == 2 or start >= end:
+        return 2
+    elif dp[start][end] == 1:
+        return 1
+    elif palindrome[start] == palindrome[end]:
+        dp[start][end] = palindrome_dp(start + 1, end - 1)
+        return dp[start][end]
     else:
-        result[start][end] = 0
-        return 0
+        return 1
 
 
-cnt = int(sys.stdin.readline())
-gogo = []
+M = int(sys.stdin.readline())
 
-for i in range(cnt):
-    left, right = map(int, sys.stdin.readline().split())
-    tmp = judge(left - 1, right - 1)
-    gogo.append(tmp)
+for i in range(M):
+    S, E = map(int, sys.stdin.readline().split())
 
-for i in gogo:
-    print(i)
+    print(1 if palindrome_dp(S-1, E-1) == 2 else 0)
