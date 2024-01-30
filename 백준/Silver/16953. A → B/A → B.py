25f5c00da49faf1e0dde5b5a5ff8ca93e8cf72ff
Seungkyu-Han <98071131+Seungkyu-Han@users.noreply.github.com>
2024-01-30 23:01:53 +0900

    [Silver II] Title: A → B, Time: 72 ms, Memory: 32148 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -1,25 +1,25 @@
 import sys
-from collections import deque
 
-start, end = map(int, sys.stdin.readline().split())
+A, B = map(int, sys.stdin.readline().split())
 
-need_visit = deque()
-need_visit.append([1, start])
-result = -1
-visited = set()
+greedy = set()
+greedy.add(A)
 
-while need_visit:
-    cur_cnt, cur = need_visit.popleft()
-    if cur == end:
-        result = cur_cnt
-        break
-    case1 = cur * 2
-    case2 = int(str(cur) + '1')
-    if case1 <= 1000000000 and case1 not in visited:
-        visited.add(case1)
-        need_visit.append([cur_cnt + 1, case1])
-    if case2 <= 1000000000 and case2 not in visited:
-        visited.add(case2)
-        need_visit.append([cur_cnt + 1, case2])
+result = 1
 
-print(result)
+while B not in greedy and greedy:
+
+    current_greedy = set()
+
+    while greedy:
+        element = greedy.pop()
+        if element * 2 <= 10 ** 9:
+            current_greedy.add(element * 2)
+        if element * 10 + 1 <= 10 ** 9:
+            current_greedy.add(element * 10 + 1)
+
+    result += 1
+    greedy = current_greedy
+
+
+print(result if greedy else -1)
