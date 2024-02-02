e2829a4add78f1d1f63667922b84d03c1e993af4
Seungkyu-Han <98071131+Seungkyu-Han@users.noreply.github.com>
2024-02-03 01:51:42 +0900

    [Gold II] Title: 보석 도둑, Time: 3404 ms, Memory: 139948 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.py
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -1,86 +1,28 @@
-import sys, heapq
-
-
-class MyHeap:
-
-    def __init__(self):
-        self.heap_list = [0] * 700000
-        self.cur_len = 0
-
-    def add(self, value):
-        self.cur_len += 1
-        self.heap_list[self.cur_len] = value
-        cur_index = self.cur_len
-        while cur_index > 1 and self.go_up(cur_index):
-            cur_index //= 2
-        return
-
-    def go_up(self, index):
-        if self.heap_list[index // 2] < self.heap_list[index]:
-            self.heap_list[index // 2], self.heap_list[index] = self.heap_list[index], self.heap_list[index // 2]
-            return True
-        else:
-            return False
-
-    def pop(self):
-        if self.cur_len < 1:
-            return False
-
-        return_value = self.heap_list[1]
-        self.heap_list[1] = self.heap_list[self.cur_len]
-        self.cur_len -= 1
-        cur_index = 1
-
-        while True:
-
-            if cur_index * 2 > self.cur_len:
-                break
-            elif cur_index * 2 == self.cur_len:
-                if self.heap_list[cur_index * 2] > self.heap_list[cur_index]:
-                    self.heap_list[cur_index * 2], self.heap_list[cur_index] = self.heap_list[cur_index], self.heap_list[cur_index * 2]
-                break
-            else:
-                if self.heap_list[cur_index * 2 + 1] > self.heap_list[cur_index * 2]:
-                    if self.heap_list[cur_index] > self.heap_list[cur_index * 2 + 1]:
-                        break
-                    else:
-                        self.heap_list[cur_index * 2 + 1], self.heap_list[cur_index] = self.heap_list[cur_index], self.heap_list[cur_index * 2 + 1]
-                        cur_index = cur_index * 2 + 1
-                else:
-                    if self.heap_list[cur_index] > self.heap_list[cur_index * 2]:
-                        break
-                    else:
-                        self.heap_list[cur_index * 2], self.heap_list[cur_index] = self.heap_list[cur_index], self.heap_list[cur_index * 2]
-                        cur_index = cur_index * 2
-        return return_value
-
-    def return_len(self):
-        return self.cur_len
-
-    def peek(self):
-        return self.heap_list[1]
-
-
-steal = []
-result = 0
+import sys
+import heapq
 
 N, K = map(int, sys.stdin.readline().split())
 
-for i in range(N):
-    heapq.heappush(steal, tuple(map(int, sys.stdin.readline().split())))
+jewelry = list()
 
-for i in range(K):
-    heapq.heappush(steal, (int(sys.stdin.readline()), 1e9))
+for _ in range(N):
+    heapq.heappush(jewelry, list(map(int, sys.stdin.readline().split())))
 
-can = MyHeap()
+for _ in range(K):
+    heapq.heappush(jewelry, [int(sys.stdin.readline().strip()), float('inf')])
 
-while steal:
-    cur_weight, cur_value = heapq.heappop(steal)
+result = 0
+cur_jewelry = list()
 
-    if cur_value != 1e9:
-        can.add(cur_value)
+while jewelry:
+
+    weight, value = heapq.heappop(jewelry)
+
+    if value == float('inf'):
+        if cur_jewelry:
+            result += heapq.heappop(cur_jewelry)
     else:
-        if can.return_len() > 0:
-            result += can.pop()
+        heapq.heappush(cur_jewelry, value * -1)
 
-print(result)
+
+print(result * -1)
