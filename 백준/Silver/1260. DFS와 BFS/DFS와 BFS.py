f42351ea88832dcca5b79a74e1ca9c271e365e52
Seungkyu-Han <98071131+Seungkyu-Han@users.noreply.github.com>
2024-02-08 13:29:13 +0900

    [Silver II] Title: DFS와 BFS, Time: 304 ms, Memory: 39432 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -2,42 +2,42 @@ import sys
 
 N, M, V = map(int, sys.stdin.readline().split())
 
-mydict = dict()
-for i in range(N):
-    mydict[i + 1] = []
+graph = [[False for _ in range(N+1)] for _ in range(N+1)]
 
 for i in range(M):
-    a, b = map(int, input().split())
-    mydict[a].append(b)
-    mydict[b].append(a)
+    V1, V2 = map(int, sys.stdin.readline().split())
+    graph[V1-1][V2-1] = True
+    graph[V2-1][V1-1] = True
 
-for_dfs = dict(mydict)
-dfs = []
-dfs_need_visit = [V]
-dfs_visited = []
+DFS, BFS = [False for i in range(N)], [False for i in range(N)]
+to_visit = [V-1]
 
-for_bfs = dict(mydict)
-bfs = []
-bfs_need_visit = [V]
-bfs_visited = []
+while to_visit:
+    cur_node = to_visit.pop(0)
+    if DFS[cur_node]:
+        continue
+    save_to_visit = []
+    for i in range(N):
+        if graph[cur_node][i] and not DFS[i]:
+            save_to_visit.append(i)
 
+    to_visit = save_to_visit + to_visit
 
-while dfs_need_visit:
-    data = dfs_need_visit.pop()
-    if data not in dfs_visited:
-        dfs_need_visit.extend(reversed(sorted(for_dfs[data])))
-        dfs_visited.append(data)
-
-while bfs_need_visit:
-    data = bfs_need_visit.pop(0)
-    if data not in bfs_visited:
-        bfs_need_visit.extend(sorted(for_bfs[data]))
-        bfs_visited.append(data)
-
-for i in dfs_visited:
-    print(i, end=' ')
+    print(cur_node + 1, end=' ')
+    DFS[cur_node] = True
 
 print()
 
-for i in bfs_visited:
-    print(i, end=' ')
+to_visit = [V-1]
+
+while to_visit:
+    cur_node = to_visit.pop(0)
+    if BFS[cur_node]:
+        continue
+
+    for i in range(N):
+        if graph[cur_node][i] and not BFS[i]:
+            to_visit.append(i)
+
+    print(cur_node + 1, end=' ')
+    BFS[cur_node] = True
