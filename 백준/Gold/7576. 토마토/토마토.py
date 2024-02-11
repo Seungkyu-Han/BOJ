233d2a354915bc8d298d42520f4d732be1cee4ee
Seungkyu-Han <98071131+Seungkyu-Han@users.noreply.github.com>
2024-02-11 16:06:03 +0900

    [Gold V] Title: 토마토, Time: 512 ms, Memory: 219544 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,41 +1,39 @@
-from collections import deque
 import sys
-import itertools
+from collections import deque
 
-M, N = map(int, sys.stdin.readline().split())
+N, M = map(int, sys.stdin.readline().split())
 
-tomato = [[None]] * N
+tomato = [list(map(int, sys.stdin.readline().split())) for _ in range(M)]
 
-end_cnt = 0
-cnt = 0
+ripe = deque()
 
-need_visit = deque()
+for m in range(M):
+    for n in range(N):
+        if tomato[m][n] == 1:
+            ripe.append([0, m, n])
 
-for i in range(N):
-    tmp_ans = list(map(int, sys.stdin.readline().split()))
-    for t in range(M):
-        if tmp_ans[t] == 0:
-            end_cnt += 1
-        elif tmp_ans[t] == 1:
-            need_visit.append((i, t))
-    tomato[i] = tmp_ans
+max_result = 0
 
-while need_visit:
-    tmp_need_visit = deque()
-    while need_visit:
-        n, m = need_visit.pop()
-        for n_ptr, m_ptr in ((n - 1, m), (n + 1, m), (n, m - 1), (n, m + 1)):
-            if 0 <= n_ptr < N and 0 <= m_ptr < M and tomato[n_ptr][m_ptr] == 0:
-                tomato[n_ptr][m_ptr] = 1
-                tmp_need_visit.append((n_ptr, m_ptr))
-    need_visit = deque(tmp_need_visit)
-    cnt += 1
+while ripe:
 
-flat = list(itertools.chain(list(itertools.chain(*tomato))))
+    cur_cnt, cur_m, cur_n = ripe.popleft()
 
-if end_cnt == 0:
-    print(0)
-elif 0 in flat:
-    print(-1)
-else:
-    print(cnt-1)
+    max_result = max(max_result, cur_cnt)
+
+    for next_m, next_n in [[cur_m, cur_n + 1], [cur_m, cur_n - 1], [cur_m + 1, cur_n], [cur_m - 1, cur_n]]:
+        if 0 <= next_m < M and 0 <= next_n < N and tomato[next_m][next_n] == 0:
+            tomato[next_m][next_n] = 1
+            ripe.append([cur_cnt + 1, next_m, next_n])
+
+
+is_finish = True
+
+for m in range(M):
+    for n in range(N):
+        if tomato[m][n] == 0:
+            is_finish = False
+            break
+    if not is_finish:
+        break
+
+print(max_result if is_finish else -1)
