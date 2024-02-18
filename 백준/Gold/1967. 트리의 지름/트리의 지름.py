8bf6547ec8caa162e4a4e3b4ae3e16ce0beecb76
Seungkyu-Han <98071131+Seungkyu-Han@users.noreply.github.com>
2024-02-18 22:13:42 +0900

    [Gold IV] Title: 트리의 지름, Time: 6988 ms, Memory: 213236 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -1,38 +1,36 @@
-from collections import deque
 import sys
+from collections import deque
 
-num = int(sys.stdin.readline())
+n = int(sys.stdin.readline().strip())
 
-if num > 1:
-    graph = {i + 1: [] for i in range(num)}
+node = {i: [] for i in range(n)}
 
-    for i in range(num - 1):
-        start, end, length = map(int, sys.stdin.readline().split())
-        graph[start].append((end, length))
-        graph[end].append((start, length))
+for i in range(n - 1):
+    start, end, weight = map(int, sys.stdin.readline().split())
 
+    node[start - 1].append([end - 1, weight])
+    node[end - 1].append([start - 1, weight])
 
-    def rad(target):
-        need_visit = deque()
-        need_visit.append(target)
-        visited = [-1] * (num + 1)
-        visited[target] = 0
-        result = [0, 0]  # len, node
-        while need_visit:
-            cur = need_visit.pop()
-            cur_list = graph[cur]
-            cur_length = visited[cur]
-            for ver, weight in cur_list:
-                if visited[ver] == -1:
-                    visited[ver] = cur_length + weight
-                    need_visit.append(ver)
-                    if visited[ver] > result[0]:
-                        result = [visited[ver], ver]
-        return result
+result = 0
 
+for i in range(n):
+    cur_result = 0
+    visited = [False for _ in range(n)]
+    need_visit = deque()
+    need_visit.append([0, i])
+    visited[i] = True
 
-    far_node = rad(1)[1]
-    print(rad(far_node)[0])
+    while need_visit:
 
-else:
-    print(0)
+        cur_weight, cur_node = need_visit.popleft()
+
+        cur_result = max(cur_result, cur_weight)
+
+        for next_node, next_weight in node[cur_node]:
+            if visited[next_node] is False:
+                visited[next_node] = True
+                need_visit.append([cur_weight + next_weight, next_node])
+
+    result = max(result, cur_result)
+
+print(result)
