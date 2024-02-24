fca5b57c06c643e4e6d0ea60f150665953f8658c
Seungkyu-Han <98071131+Seungkyu-Han@users.noreply.github.com>
2024-02-25 04:30:58 +0900

    [Silver V] Title: 너의 평점은, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,20 @@
+import sys
+
+total_credit = 0
+total_score = 0
+
+score_dict = {'A+': 4.5, 'A0': 4.0, 'B+': 3.5, 'B0': 3.0, 'C+': 2.5, 'C0': 2.0, 'D+': 1.5, 'D0': 1.0, 'F': 0.0}
+
+for _ in range(20):
+    name, credit, score = sys.stdin.readline().split()
+
+    if score == 'P':
+        continue
+
+    credit = float(credit)
+
+    total_credit += credit
+
+    total_score += (credit * score_dict[score])
+
+print(total_score / total_credit)
