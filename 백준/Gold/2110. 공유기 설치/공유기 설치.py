b78f81cb1cf0132b09819ff6734abb78146bbf31
Seungkyu-Han <98071131+Seungkyu-Han@users.noreply.github.com>
2024-03-10 15:17:18 +0900

    [Gold IV] Title: 공유기 설치, Time: 312 ms, Memory: 38848 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -9,24 +9,28 @@ for i in range(N):
 
 house.sort()
 
-start = 1
-end = house[-1] - house[0]
-result = 0
 
-while start <= end:
-    mid = (start + end) // 2
+def wifi(length):
+    result = 1
     cur = house[0]
-    cnt = 1
-
     for i in range(1, N):
-        if house[i] >= cur + mid:
-            cnt += 1
+        if house[i] >= cur + length:
             cur = house[i]
+            result += 1
+
+    return result
+
+
+left = 1
+right = house[-1] - house[0]
+
+while left <= right:
+    mid = (left + right) // 2
+    cnt = wifi(mid)
 
     if cnt >= M:
-        result = mid
-        start = mid + 1
+        left = mid + 1
     else:
-        end = mid - 1
+        right = mid - 1
 
-print(result)
+print(right)
