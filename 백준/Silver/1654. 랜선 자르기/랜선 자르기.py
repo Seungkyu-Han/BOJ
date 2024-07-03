ac0395a663ccd2327ec503c78f54ebc0e9fed5f6
한승규 <98071131+Seungkyu-Han@users.noreply.github.com>
2024-07-03 20:24:01 +0900

    [Silver II] Title: 랜선 자르기, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,31 +1,29 @@
 import sys
 
+
+def getLan(length):
+    total = 0
+
+    for i in lan:
+        total += (i // length)
+
+    return total
+
+
 K, N = map(int, sys.stdin.readline().split())
 
-lan = list()
-max_lan = 0
-
-for i in range(K):
-    cur_lan = int(sys.stdin.readline().strip())
-    lan.append(cur_lan)
-    max_lan = max(max_lan, cur_lan)
+lan = [int(sys.stdin.readline()) for i in range(K)]
 
 
-def cur_lan(length):
-    result = 0
-    for lan_length in lan:
-        result += (lan_length // length)
-    return result
-
-
-left, right = 0, max_lan
+left, right = 1, max(lan)
 
 while left <= right:
-    mid = (left + right) // 2
-    mid = max(1, mid)
-    lan_count = cur_lan(mid)
 
-    if lan_count >= N:
+    mid = (left + right) // 2
+
+    result = getLan(mid)
+
+    if result >= N:
         left = mid + 1
     else:
         right = mid - 1
