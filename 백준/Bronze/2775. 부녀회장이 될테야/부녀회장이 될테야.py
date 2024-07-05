903ef8dc3ba26b404597b55238501cbaa5d8904b
한승규 <98071131+Seungkyu-Han@users.noreply.github.com>
2024-07-06 01:15:21 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -1,12 +1,13 @@
-num = int(input())
+import sys
 
-for i in range(num):
-    k = int(input())
-    n = int(input())
-    list1 = [i+1 for i in range(n)]
-    list2 = list(list1)
-    for x in range(1, k+1):
-        for t in range(n):
-            list2[t] = sum(list1[0:t+1])
-        list1 = list(list2)
-    print(list1[n-1])
+for _ in range(int(sys.stdin.readline())):
+    k = int(sys.stdin.readline())
+    n = int(sys.stdin.readline())
+
+    house = [i + 1 for i in range(n)]
+
+    for i in range(k):
+        tmp_house = [sum(house[:i + 1]) for i in range(n)]
+        house = tmp_house
+
+    print(house[-1])
