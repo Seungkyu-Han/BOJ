e0b80b01e5eb62067edaf0537b6b0979d1f7d227
한승규 <98071131+Seungkyu-Han@users.noreply.github.com>
2024-07-10 18:26:37 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,27 +1,17 @@
 import sys
 
+stack = []
 
-def top(lists):
-    if len(lists) == 0:
-        print(-1)
-    else:
-        print(lists[len(lists)-1])
+for _ in range(int(sys.stdin.readline())):
+    command = list(map(str, sys.stdin.readline().split()))
 
-
-num = int(input())
-list1 = []
-for i in range(num):
-    input_str = list(map(str, sys.stdin.readline().split()))
-    if input_str[0] == 'push':
-        list1.append(int(input_str[1]))
-    elif input_str[0] == 'pop':
-        print(list1.pop() if len(list1) != 0 else -1)
-    elif input_str[0] == 'size':
-        print(len(list1))
-    elif input_str[0] == 'empty':
-        if len(list1) == 0:
-            print(1)
-        else:
-            print(0)
-    else:
-        top(list1)
+    if command[0] == 'push':
+        stack.append(int(command[1]))
+    elif command[0] == 'pop':
+        print(stack.pop() if stack else -1)
+    elif command[0] == 'size':
+        print(len(stack))
+    elif command[0] == 'empty':
+        print(0 if stack else 1)
+    elif command[0] == 'top':
+        print(stack[-1] if stack else -1)
