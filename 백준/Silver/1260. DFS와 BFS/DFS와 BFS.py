b9b5a2c5b77de0f42b806c716a8a27dd9e10419f
한승규 <98071131+Seungkyu-Han@users.noreply.github.com>
2024-11-15 12:05:47 +0900

    [Silver II] Title: DFS와 BFS, Time: 256 ms, Memory: 40316 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,43 +1,48 @@
 import sys
+from collections import deque
 
 N, M, V = map(int, sys.stdin.readline().split())
 
-graph = [[False for _ in range(N+1)] for _ in range(N+1)]
+graph = [[False for _ in range(N + 1)] for _ in range(N + 1)]
 
-for i in range(M):
-    V1, V2 = map(int, sys.stdin.readline().split())
-    graph[V1-1][V2-1] = True
-    graph[V2-1][V1-1] = True
+for _ in range(M):
+    v1, v2 = map(int, sys.stdin.readline().split())
+    graph[v1][v2] = True
+    graph[v2][v1] = True
 
-DFS, BFS = [False for i in range(N)], [False for i in range(N)]
-to_visit = [V-1]
+visited = [False for _ in range(N + 1)]
 
-while to_visit:
-    cur_node = to_visit.pop(0)
-    if DFS[cur_node]:
+need_visit = deque()
+need_visit.append(V)
+
+while need_visit:
+
+    v = need_visit.popleft()
+    if visited[v]:
         continue
-    save_to_visit = []
-    for i in range(N):
-        if graph[cur_node][i] and not DFS[i]:
-            save_to_visit.append(i)
+    visited[v] = True
+    print(v, end=' ')
 
-    to_visit = save_to_visit + to_visit
-
-    print(cur_node + 1, end=' ')
-    DFS[cur_node] = True
-
+    for i in range(N, 0, -1):
+        if graph[v][i] and visited[i] == False:
+            need_visit.appendleft(i)
 print()
 
-to_visit = [V-1]
 
-while to_visit:
-    cur_node = to_visit.pop(0)
-    if BFS[cur_node]:
+visited = [False for _ in range(N + 1)]
+
+need_visit = deque()
+need_visit.append(V)
+
+while need_visit:
+
+    v = need_visit.pop()
+    if visited[v]:
         continue
+    visited[v] = True
+    print(v, end=' ')
 
-    for i in range(N):
-        if graph[cur_node][i] and not BFS[i]:
-            to_visit.append(i)
-
-    print(cur_node + 1, end=' ')
-    BFS[cur_node] = True
+    for i in range(1, N + 1):
+        if graph[v][i] and visited[i] == False:
+            need_visit.appendleft(i)
+print()
