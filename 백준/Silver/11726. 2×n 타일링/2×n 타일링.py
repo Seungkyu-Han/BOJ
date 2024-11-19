fc082befa762b5d6727a094a12008ee5c7c01129
한승규 <98071131+Seungkyu-Han@users.noreply.github.com>
2024-11-20 01:47:00 +0900

    [Silver III] Title: 2×n 타일링, Time: 104 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,10 +1,8 @@
-import sys
+n = int(input())
 
-n = int(sys.stdin.readline())
+dp = [1, 2]
 
-square = [1 for i in range(n + 1)]
+for i in range(2, n):
+    dp.append(dp[i-1] + dp[i-2])
 
-for i in range(2, n + 1):
-    square[i] = square[i-1] + square[i-2]
-
-print(square[-1] % 10007)
+print(dp[n - 1] % 10007)
