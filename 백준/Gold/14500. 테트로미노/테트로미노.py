eb004b45a6d5d91227a3cb8ac32abdfa8e3e0627
한승규 <98071131+Seungkyu-Han@users.noreply.github.com>
2024-11-20 13:41:36 +0900

    [Gold IV] Title: 테트로미노, Time: 880 ms, Memory: 36560 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -2,41 +2,77 @@ import sys
 
 N, M = map(int, sys.stdin.readline().split())
 
-value = []
-for i in range(N):
-    value.append(list(map(int, sys.stdin.readline().split())))
 
-TT = (
-    ((0, 1), (0, 2), (0, 3)),
-    ((1, 0), (2, 0), (3, 0)),
-    ((0, 1), (1, 0), (1, 1)),
-    ((1, 0), (2, 0), (2, 1)),
-    ((1, 0), (2, 0), (2, -1)),
-    ((0, -1), (1, 0), (2, 0)),
-    ((0, 1), (1, 0), (2, 0)),
-    ((1, 0), (1, 1), (1, 2)),
-    ((0, 1), (0, 2), (-1, 2)),
-    ((0, 1), (0, 2), (1, 2)),
-    ((0, 1), (0, 2), (1, 0)),
-    ((1, 0), (0, 1), (-1, 1)),
-    ((1, 0), (1, 1), (2, 1)),
-    ((0, 1), (-1, 1), (-1, 2)),
-    ((0, 1), (1, 1), (1, 2)),
-    ((0, 1), (1, 1), (0, 2)),
-    ((0, 1), (-1, 1), (0, 2)),
-    ((-1, 0), (-1, -1), (-2, 0)),
-    ((-1, 0), (-1, 1), (-2, 0)),
-)
+graph = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+
+def shape1(y, x):
+    def_result = 0
+
+    if x + 3 < M:
+        def_result = max(def_result, sum(graph[y][x:x+4]))
+
+    if y + 3 < N:
+        def_result = max(def_result,
+                     graph[y][x] + graph[y+1][x] + graph[y+2][x] + graph[y+3][x])
+
+    return def_result
+
+def shape2(y, x):
+    if x + 1 < M and y + 1 < N:
+        return graph[y][x] + graph[y+1][x] + graph[y][x+1] + graph[y+1][x+1]
+    else:
+        return 0
+
+def shape3(y, x):
+
+    def_result = 0
+
+    if y + 2 < N and x + 1 < M:
+        target = (graph[y][x] + graph[y + 1][x] + graph[y + 2][x]) + (
+                    graph[y][x + 1] + graph[y + 1][x + 1] + graph[y + 2][x + 1])
+
+        cur_min = min(
+            graph[y][x] + graph[y + 1][x],
+            graph[y + 1][x] + graph[y + 2][x],
+            graph[y][x + 1] + graph[y + 1][x + 1],
+            graph[y + 1][x + 1] + graph[y + 2][x + 1],
+
+            graph[y][x + 1] + graph[y + 2][x],
+            graph[y][x] + graph[y + 2][x + 1],
+
+            graph[y][x] + graph[y + 2][x],
+            graph[y][x + 1] + graph[y + 2][x + 1]
+        )
+
+        def_result = max(def_result, target - cur_min)
+
+
+    if y + 1 < N and x + 2 < M:
+        target = (graph[y][x] + graph[y + 1][x]) + (graph[y][x + 1] + graph[y + 1][x + 1]) + (
+                    graph[y][x + 2] + graph[y + 1][x + 2])
+
+        cur_min = min(
+            graph[y + 1][x + 1] + graph[y + 1][x + 2],
+            graph[y + 1][x] + graph[y + 1][x + 1],
+            graph[y][x + 1] + graph[y][x + 2],
+            graph[y][x] + graph[y][x + 1],
+
+            graph[y][x] + graph[y + 1][x + 2],
+            graph[y + 1][x] + graph[y][x + 2],
+
+            graph[y][x] + graph[y][x + 2],
+            graph[y + 1][x] + graph[y + 1][x + 2]
+        )
+
+        def_result = max(def_result, target - cur_min)
+
+    return def_result
 
 result = 0
 
 for i in range(N):
-    for t in range(M):
-        for fi, se, th in TT:
-            fi_N, fi_M = fi
-            se_N, se_M = se
-            th_N, th_M = th
-            if 0 <= fi_N + i < N and 0 <= se_N + i < N and 0 <= th_N + i < N and 0 <= fi_M + t < M and 0 <= se_M + t < M and 0 <= th_M + t < M:
-                result = max(result, value[i][t] + value[i + fi_N][t + fi_M] + value[i + se_N][t + se_M] + value[i + th_N][t + th_M])
+    for j in range(M):
+        result = max(result, shape1(i, j), shape2(i, j), shape3(i, j))
 
 print(result)
