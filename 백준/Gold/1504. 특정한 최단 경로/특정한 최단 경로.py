64a3974947aa11c154a2ef782e74872c7aa7010b
한승규 <98071131+Seungkyu-Han@users.noreply.github.com>
2024-11-24 16:45:13 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 352 ms, Memory: 64412 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
@@ -1,64 +1,44 @@
-from collections import deque
 import sys
+import heapq
 
 N, E = map(int, sys.stdin.readline().split())
 
-graph = {i+1: [] for i in range(N)}
+graph = {n + 1: [] for n in range(N)}
 
-for i in range(E):
-    start, end, length = map(int, sys.stdin.readline().split())
-    graph[start].append((end, length))
-    graph[end].append((start, length))
+for _ in range(E):
+    v, u, weight = map(int, sys.stdin.readline().split())
 
-node1, node2 = map(int, sys.stdin.readline().split())
+    graph[v].append((u, weight))
+    graph[u].append((v, weight))
 
-start_length = [float('inf')] * (N + 1)
-start_length[1] = 0
-node1_length = [float('inf')] * (N + 1)
-node1_length[node1] = 0
-node2_length = [float('inf')] * (N + 1)
-node2_length[node2] = 0
+v1, v2 = map(int, sys.stdin.readline().split())
 
-need_visit = deque()
-need_visit.append(1)
 
-while need_visit:
-    cur = need_visit.popleft()
-    cur_list = graph[cur]
-    cur_length = start_length[cur]
-    for i in cur_list:
-        if start_length[i[0]] > cur_length + i[1]:
-            start_length[i[0]] = cur_length + i[1]
-            need_visit.append(i[0])
+def dijkstra(cur_graph, start):
+    distances = {n + 1: float('inf') for n in range(N)}
+    need_visit = []
 
-need_visit.clear()
-need_visit.append(node1)
+    heapq.heappush(need_visit, [0, start])
 
-while need_visit:
-    cur = need_visit.popleft()
-    cur_list = graph[cur]
-    cur_length = node1_length[cur]
-    for i in cur_list:
-        if node1_length[i[0]] > cur_length + i[1]:
-            node1_length[i[0]] = cur_length + i[1]
-            need_visit.append(i[0])
+    while need_visit:
 
-need_visit.clear()
-need_visit.append(node2)
+        cur_distance, cur_node = heapq.heappop(need_visit)
 
-while need_visit:
-    cur = need_visit.popleft()
-    cur_list = graph[cur]
-    cur_length = node2_length[cur]
-    for i in cur_list:
-        if node2_length[i[0]] > cur_length + i[1]:
-            node2_length[i[0]] = cur_length + i[1]
-            need_visit.append(i[0])
+        if distances[cur_node] < cur_distance:
+            continue
 
-case1 = start_length[node1] + node1_length[node2] + node2_length[N]
-case2 = start_length[node2] + node2_length[node1] + node1_length[N]
 
-if case1 == float('inf') and case2 == float('inf'):
-    print(-1)
-else:
-    print(min(case1, case2))
+        for next_node, next_distance in cur_graph[cur_node]:
+            if next_distance + cur_distance < distances[next_node]:
+                distances[next_node] = next_distance + cur_distance
+                heapq.heappush(need_visit, [next_distance + cur_distance, next_node])
+
+    distances[start] = 0
+    return distances
+
+path_v1 = dijkstra(graph, v1)
+path_v2 = dijkstra(graph, v2)
+
+result = min(path_v1[1] + path_v2[N], path_v1[N] + path_v2[1]) + path_v1[v2]
+
+print(result if result != float('inf') else -1)
