d96592987839cf3827fca1fb46ecd1b6a48fb843
한승규 <98071131+Seungkyu-Han@users.noreply.github.com>
2024-11-24 16:30:16 +0900

    [Gold III] Title: 파티, Time: 588 ms, Memory: 73024 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -3,53 +3,48 @@ import heapq
 
 N, M, X = map(int, sys.stdin.readline().split())
 
-graph = {i: [] for i in range(N+1)}
+graph = [[float('inf') for _ in range(N)] for _ in range(N)]
 
-for i in range(M):
-    start, end, weight = map(int, sys.stdin.readline().split())
-    graph[start].append([end, weight])
+for _ in range(M):
+    v, u, weight = map(int, sys.stdin.readline().split())
+    graph[v-1][u-1] = weight
 
-result = [0] * (N + 1)
+go_to_X = {i:float('inf') for i in range(N)}
+go_from_X = {i:float('inf') for i in range(N)}
+go_to_X[X - 1], go_from_X[X - 1] = 0, 0
 
-for i in range(1, N + 1):
-    if N == X:
-        continue
-    dijk = [float('inf')] * (N + 1)
-    dijk[i] = 0
-    need_visit = []
-    heapq.heappush(need_visit, [0, i]) # dist, node
-    while need_visit:
-
-        cur_dist, cur_node = heapq.heappop(need_visit)
-
-        if cur_dist > dijk[cur_node]:
-            continue
-
-        for end_node, dist in graph[cur_node]:
-            distance = dist + cur_dist
-            if distance < dijk[end_node]:
-                dijk[end_node] = distance
-                heapq.heappush(need_visit, [distance, end_node])
-
-    result[i] = dijk[X]
-
-dijk = [float('inf')] * (N + 1)
-dijk[X] = 0
 need_visit = []
-heapq.heappush(need_visit, [0, X])
-while need_visit:
-    cur_dist, cur_node = heapq.heappop(need_visit)
+heapq.heappush(need_visit, [0, X - 1])
 
-    if cur_dist > dijk[cur_node]:
+while need_visit:
+    cur_weight, cur_node = heapq.heappop(need_visit)
+
+    if cur_weight > go_to_X[cur_node]:
         continue
 
-    for end_node, dist in graph[cur_node]:
-        distance = dist + cur_dist
-        if distance < dijk[end_node]:
-            dijk[end_node] = distance
-            heapq.heappush(need_visit, [distance, end_node])
+    for i in range(N):
+        if go_to_X[i] > cur_weight + graph[i][cur_node]:
+            go_to_X[i] = cur_weight + graph[i][cur_node]
+            heapq.heappush(need_visit, (cur_weight + graph[i][cur_node], i))
 
-for i in range(1, N + 1):
-    result[i] += dijk[i]
-    
-print(max(result))
+
+heapq.heappush(need_visit, [0, X - 1])
+
+while need_visit:
+
+    cur_weight, cur_node = heapq.heappop(need_visit)
+
+    if cur_weight > go_from_X[cur_node]:
+        continue
+
+    for i in range(N):
+        if go_from_X[i] > cur_weight + graph[cur_node][i]:
+            go_from_X[i] = cur_weight + graph[cur_node][i]
+            heapq.heappush(need_visit, (cur_weight + graph[cur_node][i], i))
+
+result = 0
+
+for i in range(N):
+    result = max(result, go_from_X[i] + go_to_X[i])
+
+print(result)
