e50c22ccb63b64208ec5fb488aceb9a871e13f91
한승규 <98071131+Seungkyu-Han@users.noreply.github.com>
2024-11-29 15:17:15 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 2372 ms, Memory: 448180 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -2,67 +2,34 @@ import sys
 
 sys.setrecursionlimit(10**6)
 
-
-class Tree:
-    class Node:
-        def __init__(self, data, left=None, right=None):
-            self.data = data
-            self.left = left
-            self.right = right
-
-    def __init__(self):
-        self.head = None
-
-    def make_head(self, data):
-        self.head = self.Node(data)
-
-    def insert(self, data):
-        cur = self.head
-        while True:
-            if data < cur.data:
-                if cur.left is None:
-                    cur.left = self.Node(data)
-                    return
-                else:
-                    cur = cur.left
-            else:
-                if cur.right is None:
-                    cur.right = self.Node(data)
-                    return
-                else:
-                    cur = cur.right
-    result = []
-
-    def to_inorder(self, target):
-        if target.left is not None:
-            self.to_inorder(target.left)
-        if target.right is not None:
-            self.to_inorder(target.right)
-        self.result.append(target.data)
-        return
-
-    def inorder(self):
-        self.result.clear()
-        cur = self.head
-        self.to_inorder(cur)
-        return self.result
-
-
-input_data = []
+tree = []
 
 while True:
     try:
-        input_data.append(int(sys.stdin.readline()))
+        tree.append(int(sys.stdin.readline()))
     except:
         break
 
-myTree = Tree()
-myTree.make_head(input_data[0])
+def suf(cur_list):
 
-for i in input_data[1:]:
-    myTree.insert(i)
+    if len(cur_list) < 1:
+        return
 
-myresult = myTree.inorder()
+    left, right = [], []
 
-for i in myresult:
-    print(i)
+    target = cur_list[0]
+
+    for node in cur_list[1:]:
+        if node < target:
+            left.append(node)
+        else:
+            right.append(node)
+
+    if len(left) > 0:
+        suf(left)
+    if len(right) > 0:
+        suf(right)
+    print(target)
+
+
+suf(tree)
