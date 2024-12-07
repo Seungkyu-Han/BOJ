d835d3678b6f1e98fa2aef8b0c1a8abb6a4c0157
한승규 <98071131+Seungkyu-Han@users.noreply.github.com>
2024-12-07 20:36:33 +0900

    [Silver III] Title: N과 M (2), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,15 +1,19 @@
-N, M = map(int, input().split())
+import sys
 
-result = []
+N, M = map(int, sys.stdin.readline().split())
 
-def myresult(start):
-    if len(result) == M:
-        print(*result)
+
+def back_tracking(cur_numbers, n, m):
+
+    if len(cur_numbers) == m + 1:
+        for number in cur_numbers[1:]:
+            print(number, end=' ')
+        print()
         return
 
-    for i in range(start, N + 1):
-        if i not in result:
-            result.append(i)
-            myresult(i + 1)
-            result.pop()
-myresult(1)
+    for i in range(cur_numbers[-1] + 1, n + 1):
+        cur_numbers.append(i)
+        back_tracking(cur_numbers, n, m)
+        cur_numbers.pop()
+
+back_tracking([0], N, M)
