345689923276c7b9eccb8b0ab5ebc8ae6afd437d
한승규 <98071131+Seungkyu-Han@users.noreply.github.com>
2024-12-07 11:31:58 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1428 ms, Memory: 129488 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -2,23 +2,19 @@ import sys
 
 N, M = map(int, sys.stdin.readline().split())
 
-pan = []
-hap = []
+table = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
-for i in range(N):
-    pan.append(list(map(int, sys.stdin.readline().split())))
+dp = [[0 for _ in range(N + 1)] for _ in range(N)]
 
-for i in range(M):
-    hap.append(list(map(int, sys.stdin.readline().split())))
+for i1 in range(N):
+    for i2 in range(1, N + 1):
+        dp[i1][i2] = dp[i1][i2 - 1] + table[i1][i2 - 1]
 
-dp = [[0] * (N+1) for i in range(N+1)]
+for _ in range(M):
+    x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
 
-for n in range(N):
-    for m in range(N):
-        dp[n+1][m+1] = dp[n+1][m] + dp[n][m+1] - dp[n][m] + pan[n][m]
+    result = 0
+    for x in range(x1 - 1, x2):
+        result += (dp[x][y2] - dp[x][y1 - 1])
 
-
-for i in range(M):
-    start_n, start_m, end_n, end_m = hap[i]
-    result = dp[end_n][end_m] - dp[end_n][start_m - 1] - dp[start_n - 1][end_m] + dp[start_n-1][start_m-1]
     print(result)
