1474b0209d7c4c31f61031d0f487ea43625d3e53
한승규 <98071131+Seungkyu-Han@users.noreply.github.com>
2024-12-08 10:50:15 +0900

    [Gold IV] Title: 연구소, Time: 788 ms, Memory: 117056 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,62 +1,41 @@
-from collections import deque
 import sys
-import copy
+from collections import deque
 
 N, M = map(int, sys.stdin.readline().split())
 
-lab = []
-virus = []
+graph = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
-for i in range(N):
-    lab.append(list(map(int, sys.stdin.readline().split())))
-    for t in range(M):
-        if lab[-1][t] == 2:
-            virus.append((i, t))
+def bfs(cur_graph, n, m):
+    bfs_cur_graph = [row[:] for row in cur_graph]
 
-result = 0
+    for cur_n in range(n):
+        for cur_m in range(m):
+            if bfs_cur_graph[cur_n][cur_m] == 2:
+                need_visit = deque([[cur_n, cur_m]])
+                while need_visit:
+                    visited_n, visited_m = need_visit.popleft()
 
+                    for next_n, next_m in [[visited_n - 1, visited_m], [visited_n + 1, visited_m], [visited_n, visited_m - 1], [visited_n, visited_m + 1]]:
+                        if 0 <= next_n < n and 0 <= next_m < m and bfs_cur_graph[next_n][next_m] == 0:
+                            bfs_cur_graph[next_n][next_m] = 2
+                            need_visit.append([next_n, next_m])
 
-def bfs():
-    tmp = copy.deepcopy(lab)
-    for n, m in virus:
-
-        need_visit = deque()
-        need_visit.append((n, m))
-
-        while need_visit:
-
-            cur_n, cur_m = need_visit.popleft()
-
-            tmp[cur_n][cur_m] = 2
-
-            for n_ptr, m_ptr in ((cur_n + 1, cur_m), (cur_n-1, cur_m), (cur_n, cur_m-1), (cur_n, cur_m+1)):
-                if 0 <= n_ptr < N and 0 <= m_ptr < M and tmp[n_ptr][m_ptr] == 0:
-                    tmp[n_ptr][m_ptr] = 2
-                    need_visit.append((n_ptr, m_ptr))
-
-    cur = 0
-    for q in range(N):
-        for k in range(M):
-            if tmp[q][k] == 0:
-                cur += 1
-
-    global result
-    result = max(result, cur)
+    return sum(row.count(0) for row in bfs_cur_graph)
 
 
-def solve(n):
-    if n == 3:
-        bfs()
-        return
+def back_tracking(cur_graph, cur_n, cur_m, cur_wall, n, m):
 
-    for q in range(N):
-        for k in range(M):
-            if lab[q][k] == 0:
-                lab[q][k] = 1
-                solve(n+1)
-                lab[q][k] = 0
+    if cur_wall == 3:
+        return bfs(cur_graph, n, m)
 
+    result = 0
 
-solve(0)
+    for next_n in range(cur_n, n):
+        for next_m in range(cur_m if next_n == cur_n else 0, m):
+            if cur_graph[next_n][next_m] == 0:
+                cur_graph[next_n][next_m] = 1
+                result = max(result, back_tracking(cur_graph, next_n, next_m, cur_wall + 1, n, m))
+                cur_graph[next_n][next_m] = 0
+    return result
 
-print(result)
+print(back_tracking(graph, 0, 0, 0, N, M))
