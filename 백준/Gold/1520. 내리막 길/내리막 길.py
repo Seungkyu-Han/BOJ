a8d344c959c05071ed996b26d4c34483567c45fc
한승규 <98071131+Seungkyu-Han@users.noreply.github.com>
2025-01-10 17:21:24 +0900

    [Gold III] Title: 내리막 길, Time: 116 ms, Memory: 45740 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -1,28 +1,42 @@
 import sys
 
-N, M = map(int, sys.stdin.readline().split())
+sys.setrecursionlimit(10 ** 6)
 
-board = []
-result = [[-1] * M for i in range(N)]
-result[-1][-1] = 1
+# 입력 처리
+M, N = map(int, sys.stdin.readline().split())
+down_stair = [list(map(int, sys.stdin.readline().split())) for _ in range(M)]
 
-for i in range(N):
-    board.append(list(map(int, sys.stdin.readline().split())))
+# visited 배열: -1로 초기화하여 아직 계산되지 않았음을 표시
+visited = [[-1 for _ in range(N)] for _ in range(M)]
 
-
-def solve(cur_n, cur_m):
-    if result[cur_n][cur_m] != -1:
-        return result[cur_n][cur_m]
-
-    res = 0
-
-    for x_ptr, y_ptr in ((cur_n - 1, cur_m), (cur_n + 1, cur_m), (cur_n, cur_m - 1), (cur_n, cur_m + 1)):
-        if 0 <= x_ptr < N and 0 <= y_ptr < M and board[cur_n][cur_m] > board[x_ptr][y_ptr]:
-            res += solve(x_ptr, y_ptr)
-
-    result[cur_n][cur_m] = res
-    return result[cur_n][cur_m]
+# 상하좌우 방향 정의
+directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 
-solve(0, 0)
-print(result[0][0])
+# DFS 함수
+def dfs(x, y):
+    # 이미 방문한 경우 값 반환 (메모이제이션)
+    if visited[x][y] != -1:
+        return visited[x][y]
+
+    # 도착 지점에 도달한 경우
+    if x == M - 1 and y == N - 1:
+        return 1
+
+    # 초기화
+    visited[x][y] = 0
+
+    # 가능한 방향으로 이동
+    for dx, dy in directions:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < M and 0 <= ny < N and down_stair[x][y] > down_stair[nx][ny]:
+            visited[x][y] += dfs(nx, ny)
+
+    return visited[x][y]
+
+
+# 시작점에서 탐색 시작
+dfs(0, 0)
+
+# 결과 출력
+print(visited[0][0])
