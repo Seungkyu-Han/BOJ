451482d92634ff6a36d87f2ad4659f36777e12c1
한승규 <98071131+Seungkyu-Han@users.noreply.github.com>
2025-04-05 12:41:45 +0900

    [level 3] Title: 카운트 다운, Time: 154.62 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/3/131129. 카운트 다운/카운트 다운.java
+++ 프로그래머스/3/131129. 카운트 다운/카운트 다운.java
@@ -1,57 +1,67 @@
 import java.util.*;
+import java.lang.Math;
 
 class Solution {
     public int[] solution(int target) {
-        int[][] dp = new int[target + 1][2];
+        
+        int[][] dp = new int[target + 1][];
         for (int i = 0; i <= target; i++) {
-            dp[i][0] = Integer.MAX_VALUE; // 최소 다트 수
-            dp[i][1] = Integer.MIN_VALUE; // 최대 싱글/불
+           dp[i] = new int[]{1000000, 1000000};
         }
-
-        dp[target][0] = 0;
-        dp[target][1] = 0;
-
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(target);
-
-        while (!queue.isEmpty()) {
-            int score = queue.poll();
-            int dartCount = dp[score][0];
-            int hitCount = dp[score][1];
-
-            // 1~20, 곱 1~3
-            for (int i = 1; i <= 20; i++) {
-                for (int mul = 1; mul <= 3; mul++) {
-                    int nextScore = score - i * mul;
-                    if (nextScore < 0) continue;
-
-                    int nextDart = dartCount + 1;
-                    int nextHit = hitCount + (mul == 1 ? 1 : 0);
-
-                    if (dp[nextScore][0] > nextDart || 
-                        (dp[nextScore][0] == nextDart && dp[nextScore][1] < nextHit)) {
-                        dp[nextScore][0] = nextDart;
-                        dp[nextScore][1] = nextHit;
-                        queue.add(nextScore);
+        
+        Queue<Integer> count_heap = new LinkedList<>();
+        count_heap.add(0);
+        Queue<Integer> hit_heap = new LinkedList<>();
+        hit_heap.add(0);
+        Queue<Integer> score_heap = new LinkedList<>();
+        score_heap.add(target);
+        
+        while (count_heap.size() > 0){
+            int cur_count = count_heap.poll();
+            int cur_hit = hit_heap.poll();
+            int cur_score = score_heap.poll();
+            
+            int next_count = cur_count + 1;
+            
+            for(int i = 1; i <= 20; i++){
+                for(int ii = 1; ii <= 3; ii++){
+                    int next_hit = (ii == 1) ? cur_hit + 1 : cur_hit;
+                    int next_score = cur_score - (i * ii);
+                    
+                    if(next_score < 0)
+                        continue;
+                    
+                    if (dp[next_score][0] > next_count || 
+                        (dp[next_score][0] == next_count && dp[next_score][1] < next_hit)){
+                        dp[next_score][0] = next_count;
+                        dp[next_score][1] = next_hit;
+                        
+                        count_heap.add(next_count);
+                        hit_heap.add(next_hit);
+                        score_heap.add(next_score);
                     }
                 }
             }
+            
+            int next_hit = cur_hit + 1;
+            int next_score = cur_score - 50;
+                    
+            if(next_score < 0)
+                continue;
 
-            // bull (50점)
-            int nextScore = score - 50;
-            if (nextScore >= 0) {
-                int nextDart = dartCount + 1;
-                int nextHit = hitCount + 1;
+            if(dp[next_score][0] > next_count ||
+              (dp[next_score][0] == next_count || 
+                (dp[next_score][0] == next_count && dp[next_score][1] < next_hit))){
+                dp[next_score][0] = next_count;
+                dp[next_score][1] = next_hit;
 
-                if (dp[nextScore][0] > nextDart || 
-                    (dp[nextScore][0] == nextDart && dp[nextScore][1] < nextHit)) {
-                    dp[nextScore][0] = nextDart;
-                    dp[nextScore][1] = nextHit;
-                    queue.add(nextScore);
-                }
+                count_heap.add(next_count);
+                hit_heap.add(next_hit);
+                score_heap.add(next_score);
             }
         }
-
+        
+        
         return dp[0];
     }
 }
