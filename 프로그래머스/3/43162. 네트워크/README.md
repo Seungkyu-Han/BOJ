54763207d095607282a3860897bcc6eb7b0a3a9f
한승규 <98071131+Seungkyu-Han@users.noreply.github.com>
2025-04-16 16:51:00 +0900

    [level 3] Title: 네트워크, Time: 0.48 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=python3) 
 
 ### 성능 요약
 
-메모리: 4.16 MB, 시간: 0.08 ms
+메모리: 9.31 MB, 시간: 0.48 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 16일 16:27:59
+2025년 04월 16일 16:50:58
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,33 +1,39 @@
 def solution(n, computers):
-    
-    parents = [i for i in range(n)]
-    ranks = [0 for i in range(n)]
-    
-    for i in range(n):
-        for j in range(i + 1, n):
-            if computers[i][j] == 1:
-                if find(i, parents) != find(j, parents):
-                    union(i, j, parents, ranks)
+    answer = 0
     
     parent_set = set()
     
+    rank = [0 for _ in range(n)]
+    parent = [i for i in range(n)]
+    
     for i in range(n):
-        parent_set.add(find(i, parents))
+        for j in range(i + 1, n):
+            
+            if computers[i][j] == 0:
+                continue
+            
+            union(i, j, parent, rank)
+    
+    for i in range(n):
+        parent_set.add(find(i, parent))
+    
     
     return len(parent_set)
 
-def find(node, parent):
-    if parent[node] != node:
+def find(node: int, parent: list):
+    if node != parent[node]:
         parent[node] = find(parent[node], parent)
     return parent[node]
 
-def union(node1, node2, parent, rank):
-    root1 = find(node1, parent)
-    root2 = find(node2, parent)
+def union(node1: int, node2: int, parent: list, rank: list):
+    root1: int = find(node1, parent)
+    root2: int = find(node2, parent)
     
     if rank[root1] > rank[root2]:
         parent[root2] = root1
     else:
+        parent[root1] = root2
         if rank[root1] == rank[root2]:
             rank[root2] += 1
-        parent[root1] = root2
+    
+    
